4039c1b9b23377f1d2a14c9d5d18fc901835f2de
Ivan Sim <ihcsim@gmail.com>
2022-07-18 11:29:30 -0700

    Implement ServeMux in mock HTTP server
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- cmd/mock/http/main.go
+++ cmd/mock/http/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/ihcsim/cbt-aggapi/pkg/apis/cbt/v1alpha1"
@@ -13,7 +15,7 @@ import (
 	cbtgrpc "github.com/ihcsim/cbt-aggapi/pkg/grpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"k8s.io/apimachinery/pkg/api/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -21,52 +23,36 @@ import (
 	"k8s.io/klog"
 )
 
-var listenAddr = flag.String("listen", ":8080", "Address of the HTTP server")
+var (
+	listenAddr = flag.String("listen-addr", ":8080", "Address to listen at")
+	grpcTarget = flag.String("grpc-target", ":9779", "Address of the GRPC server")
+)
 
 func main() {
 	if _, err := registerDriver(); err != nil {
 		klog.Fatal(err)
 	}
 
-	grpcClient, err := grpcClient()
+	server, err := newServer()
 	if err != nil {
 		klog.Fatal(err)
 	}
 
-	http.HandleFunc("/", func(res http.ResponseWriter, r *http.Request) {
-		var (
-			ctx, cancel = context.WithTimeout(context.Background(), time.Second*180)
-			grpcReq     = &cbtgrpc.VolumeSnapshotDeltaRequest{}
-		)
-		defer cancel()
-
-		grpcRes, err := grpcClient.ListVolumeSnapshotDeltas(ctx, grpcReq)
-		if err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-			return
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	go func() {
+		<-sigChan
+		klog.Info("shutting down server")
+		if err := server.Shutdown(context.Background()); err != nil {
+			klog.Error(err)
 		}
+	}()
 
-		blockDeltas := []*v1alpha1.ChangedBlockDelta{}
-		for _, cbd := range grpcRes.GetBlockDelta().GetChangedBlockDeltas() {
-			blockDeltas = append(blockDeltas, &v1alpha1.ChangedBlockDelta{
-				Offset:         cbd.GetOffset(),
-				BlockSizeBytes: cbd.GetBlockSizeBytes(),
-				DataToken: v1alpha1.DataToken{
-					Token:        cbd.GetDataToken().GetToken(),
-					IssuanceTime: metav1.NewTime(cbd.GetDataToken().GetIssuanceTime().AsTime()),
-					TTL: metav1.Duration{
-						Duration: cbd.GetDataToken().GetTtlSeconds().AsDuration(),
-					},
-				},
-			})
-			klog.Infof("found changed block at offset %d (%d)\n", cbd.GetOffset(), cbd.GetBlockSizeBytes())
+	klog.Info("listening at: ", *listenAddr)
+	if err := server.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			klog.Error(err)
 		}
-
-		writeResponse(res, blockDeltas)
-	})
-
-	if err := http.ListenAndServe(*listenAddr, nil); err != nil {
-		klog.Fatal(err)
 	}
 }
 
@@ -103,7 +89,7 @@ func registerDriver() (runtime.Object, error) {
 		created, createErr = clientset.CbtV1alpha1().DriverDiscoveries().Create(ctx,
 			endpoint,
 			metav1.CreateOptions{})
-		if err == nil || errors.IsAlreadyExists(createErr) {
+		if err == nil || apierrors.IsAlreadyExists(createErr) {
 			klog.Infof("successfully registered CSI driver %s", endpoint.GetName())
 			createErr = nil
 			cancel()
@@ -115,31 +101,78 @@ func registerDriver() (runtime.Object, error) {
 	return created, createErr
 }
 
-func grpcClient() (cbtgrpc.VolumeSnapshotDeltaServiceClient, error) {
-	grpcTarget, exists := os.LookupEnv("GRPC_TARGET")
-	if !exists {
-		grpcTarget = ":9779"
+func newServer() (*http.Server, error) {
+	serveMux, err := newServeMux(*grpcTarget)
+	if err != nil {
+		klog.Fatal(err)
 	}
 
+	return &http.Server{
+		Addr:    *listenAddr,
+		Handler: serveMux,
+	}, nil
+}
+
+type serveMux struct {
+	grpc *grpc.ClientConn
+	*http.ServeMux
+}
+
+func newServeMux(grpcTarget string) (*serveMux, error) {
 	opts := grpc.WithTransportCredentials(insecure.NewCredentials())
 	clientConn, err := grpc.Dial(grpcTarget, opts)
 	if err != nil {
 		return nil, err
 	}
 
-	return cbtgrpc.NewVolumeSnapshotDeltaServiceClient(clientConn), nil
+	s := &serveMux{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", s.handle)
+
+	s.grpc = clientConn
+	s.ServeMux = mux
+	return s, nil
 }
 
-func writeResponse(resp http.ResponseWriter, data interface{}) {
-	body, err := json.Marshal(data)
+func (s *serveMux) handle(res http.ResponseWriter, r *http.Request) {
+	var (
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second*180)
+		grpcReq     = &cbtgrpc.VolumeSnapshotDeltaRequest{}
+	)
+	defer cancel()
+
+	client := cbtgrpc.NewVolumeSnapshotDeltaServiceClient(s.grpc)
+	grpcRes, err := client.ListVolumeSnapshotDeltas(ctx, grpcReq)
 	if err != nil {
-		http.Error(resp, err.Error(), http.StatusInternalServerError)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	resp.Header().Set("Content-Type", "application/json")
-	resp.WriteHeader(http.StatusOK)
-	if _, err := resp.Write(body); err != nil {
+	blockDeltas := []*v1alpha1.ChangedBlockDelta{}
+	for _, cbd := range grpcRes.GetBlockDelta().GetChangedBlockDeltas() {
+		blockDeltas = append(blockDeltas, &v1alpha1.ChangedBlockDelta{
+			Offset:         cbd.GetOffset(),
+			BlockSizeBytes: cbd.GetBlockSizeBytes(),
+			DataToken: v1alpha1.DataToken{
+				Token:        cbd.GetDataToken().GetToken(),
+				IssuanceTime: metav1.NewTime(cbd.GetDataToken().GetIssuanceTime().AsTime()),
+				TTL: metav1.Duration{
+					Duration: cbd.GetDataToken().GetTtlSeconds().AsDuration(),
+				},
+			},
+		})
+		klog.Infof("found changed block at offset %d (%d)\n", cbd.GetOffset(), cbd.GetBlockSizeBytes())
+	}
+
+	body, err := json.Marshal(blockDeltas)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	if _, err := res.Write(body); err != nil {
 		klog.Error(err)
 		return
 	}
